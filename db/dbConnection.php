d3628ed9f1bae5199bba1cf84b2a9488125cba00
Tolulope Adekunte <toluadekunte@gmail.com>
2021-11-17 02:01:31 +0100

    db

--- db/dbConnection.php
+++ db/dbConnection.php
@@ -4,10 +4,10 @@
 //setting variables for offline and online respectively
 if (!checkdnsrr($_SERVER['SERVER_NAME'], 'NS')){
     //echo "Website is OFFLINE";
-    $conn = mysqli_connect("localhost","root","","starwars");
+    $conn = mysqli_connect("us-cdbr-east-04.cleardb.com","b1b8f2d5478ff7","0d48068f","heroku_2c9e890d14d9800");
 }else{
     //echo "ONLINE ONLINE ONLINE";
-    $conn = mysqli_connect("localhost","orangeca_user","Orange_123@Card","orangeca_db");
+    $conn = mysqli_connect("us-cdbr-east-04.cleardb.com","b1b8f2d5478ff7","0d48068f","heroku_2c9e890d14d9800");
 }
 
 
