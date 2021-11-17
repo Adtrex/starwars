287a0b1031b978f93dc888dfd8ae32c0573c8448
Tolulope Adekunte <toluadekunte@gmail.com>
2021-11-17 02:14:18 +0100

    payment

--- access.php
+++ access.php
@@ -4,7 +4,7 @@ include_once('db/dbConnection.php');
 //Code to see if Table Exists
 
 $email = $_SESSION['email'];
-$query = "SELECT * FROM users WHERE email='$email'";
+$query = "SELECT * FROM payment WHERE email='$email'";
 $result = $conn->query($query);
 
 $row = $result->fetch_assoc();
